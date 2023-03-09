5d0aa364f676a7dffd6e3f1a8203a770f66f2f57
cpucorecore <cpucorecore@gmail.com>
2023-03-09 14:46:22 +0800

    add makefile; update command_executor; update git ignore file

--- command_executor.go
+++ command_executor.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"os/exec"
 )
 
-func DoCmd(cmd string, args []string) ([]byte, error) {
-	return exec.Command(cmd, args...).Output()
+func DoCmd(cmdName string, args []string) ([]byte, error) {
+	fmt.Printf("DoCmd:%s with %v\n", cmdName, args)
+
+	cmd := exec.Command(cmdName, args...)
+
+	var stdoutBuf bytes.Buffer
+	cmd.Stdout = &stdoutBuf
+
+	e := cmd.Run()
+	if e != nil {
+		fmt.Println(e)
+		return nil, e
+	}
+
+	return stdoutBuf.Bytes(), nil
 }
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	state, e := ClusterStateExport()
+	if e != nil {
+		fmt.Printf("ClusterStateExport err:%s\n", e.Error())
+		return
+	}
+
+	pinset, e := IpfsPinLs()
+	if e != nil {
+		fmt.Printf("IpfsPinLs err:%s\n", e.Error())
+		return
+	}
+
+	fmt.Println("cluster state:")
+	for _, pin := range state {
+		fmt.Printf("%s:%v\n", pin.Cid, pin.Allocations)
+	}
+
+	fmt.Println("ipfs pinset:")
+	for _, cid := range pinset {
+		fmt.Println(cid)
+	}
 }
